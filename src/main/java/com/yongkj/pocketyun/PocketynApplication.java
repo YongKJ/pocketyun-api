b49eab80df33b71e9be12151b90d8bcb9c8ba201
dengxiaojun <dxj1718874198@gmail.com>
2022-08-17 22:46:59 +0800

    [build]项目结构初始化

---
+++ src/main/java/com/yongkj/pocketyun/PocketynApplication.java
@@ -0,0 +1,13 @@
+package com.yongkj.pocketyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketynApplication.class, args);
+    }
+
+}
